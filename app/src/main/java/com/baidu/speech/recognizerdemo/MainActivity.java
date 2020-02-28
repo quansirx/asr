6ed23ddfe1d08881e55a1be72758e31ef351161a
quanjiamin <quanjiamin@qq.com>
2020-02-28 10:43:32 +0800

    first commit

---
+++ app/src/main/java/com/baidu/speech/recognizerdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.baidu.speech.recognizerdemo;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniRecog;
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniUnit;
+
+import com.baidu.aip.asrwakeup3.core.mini.ActivityMiniWakeUp;
+import com.baidu.aip.asrwakeup3.core.mini.AutoCheck;
+
+import java.util.EventListener;
+
+
+//MainActivity继承于以上3个活动
+
+
+public class MainActivity extends ActivityMiniRecog {
+
+}
+
+
+
